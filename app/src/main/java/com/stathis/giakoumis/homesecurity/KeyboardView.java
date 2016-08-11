bd7a3705f93b1750add9c0ac76ce79b20766c8e1
stathis <giakoumis.stathis@gmail.com>
2016-08-12 01:46:11 +0300

    Android App with normal layout and fixed bug with empty pass

---
+++ app/src/main/java/com/stathis/giakoumis/homesecurity/KeyboardView.java
@@ -0,0 +1,95 @@
+package com.stathis.giakoumis.homesecurity;
+
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.text.Editable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+/**
+ * Created by vallia on 11/8/2016.
+ */
+public class KeyboardView extends FrameLayout implements View.OnClickListener {
+
+
+    private EditText mPasswordField;
+
+    public KeyboardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public KeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public KeyboardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.keyboard, this);
+        initViews();
+    }
+
+    private void initViews() {
+        mPasswordField = (EditText)findViewById(R.id.password_field);
+        $(R.id.t9_key_0).setOnClickListener(this);
+        $(R.id.t9_key_1).setOnClickListener(this);
+        $(R.id.t9_key_2).setOnClickListener(this);
+        $(R.id.t9_key_3).setOnClickListener(this);
+        $(R.id.t9_key_4).setOnClickListener(this);
+        $(R.id.t9_key_5).setOnClickListener(this);
+        $(R.id.t9_key_6).setOnClickListener(this);
+        $(R.id.t9_key_7).setOnClickListener(this);
+        $(R.id.t9_key_8).setOnClickListener(this);
+        $(R.id.t9_key_9).setOnClickListener(this);
+        $(R.id.t9_key_clear).setOnClickListener(this);
+        $(R.id.t9_key_backspace).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // handle number button click
+        if (v.getTag() != null && "number_button".equals(v.getTag())) {
+            mPasswordField.append(((TextView) v).getText());
+            Log.i("Keyboar",mPasswordField.getText().toString());
+            return;
+        }
+        switch (v.getId()) {
+            case R.id.t9_key_clear: { // handle clear button
+                mPasswordField.setText(null);
+            }
+            break;
+            case R.id.t9_key_backspace: { // handle backspace button
+                // delete one character
+                Editable editable = mPasswordField.getText();
+                int charCount = editable.length();
+                if (charCount > 0) {
+                    editable.delete(charCount - 1, charCount);
+                }
+            }
+            case R.id.t9_key_submin: { // handle backspace button
+                // return password
+
+            }
+
+            break;
+        }
+    }
+
+    public String getInputText() {
+        Log.i("Keyboar returned", mPasswordField.getText().toString());
+        return mPasswordField.getText().toString();
+    }
+
+    protected <T extends View> T $(@IdRes int id) {
+        return (T) super.findViewById(id);
+    }
+}
--- app/src/main/java/com/stathis/giakoumis/homesecurity/PasswordActivity.java
+++ app/src/main/java/com/stathis/giakoumis/homesecurity/PasswordActivity.java
@@ -2,9 +2,13 @@ package com.stathis.giakoumis.homesecurity;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.security.keystore.KeyNotYetValidException;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,31 +23,89 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class PasswordActivity extends AppCompatActivity {
+public class PasswordActivity extends AppCompatActivity implements View.OnClickListener{
 
 
-    EditText password;
-    Button  btn_submit;
+   // EditText password;
+   // Button  btn_submit;
+   // KeyboardView myKeyboard;
+   EditText mPasswordField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_password);
+        setContentView(R.layout.keyboard);
+        initViews();
 
-        password = (EditText) findViewById(R.id.password_input);
-        btn_submit = (Button) findViewById(R.id.button);
+       //   myKeyboard = new KeyboardView(this);
 
-       btn_submit.setOnClickListener(new View.OnClickListener() {
+
+
+
+     /*  btn_submit.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-              new GetPassword().execute(password.getText().toString());
+             Toast toast = Toast.makeText(getApplicationContext(), myKeyboard.getInputText(), Toast.LENGTH_LONG);
+               toast.show();
+           //   new GetPassword().execute(myKeyboard.getInputText());
 
 
 
            }
        });
 
-
+     */
     }
+
+    private void initViews() {
+        mPasswordField = (EditText)findViewById(R.id.password_field);
+        $(R.id.t9_key_0).setOnClickListener(this);
+        $(R.id.t9_key_1).setOnClickListener(this);
+        $(R.id.t9_key_2).setOnClickListener(this);
+        $(R.id.t9_key_3).setOnClickListener(this);
+        $(R.id.t9_key_4).setOnClickListener(this);
+        $(R.id.t9_key_5).setOnClickListener(this);
+        $(R.id.t9_key_6).setOnClickListener(this);
+        $(R.id.t9_key_7).setOnClickListener(this);
+        $(R.id.t9_key_8).setOnClickListener(this);
+        $(R.id.t9_key_9).setOnClickListener(this);
+        $(R.id.t9_key_clear).setOnClickListener(this);
+        $(R.id.t9_key_backspace).setOnClickListener(this);
+        $(R.id.t9_key_submin).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // handle number button click
+        if (v.getTag() != null && "number_button".equals(v.getTag())) {
+            mPasswordField.append(((TextView) v).getText());
+            Log.i("Keyboar",mPasswordField.getText().toString());
+            return;
+        }
+        switch (v.getId()) {
+            case R.id.t9_key_clear: { // handle clear button
+                mPasswordField.setText(null);
+            }
+            break;
+            case R.id.t9_key_backspace: { // handle backspace button
+                // delete one character
+                Editable editable = mPasswordField.getText();
+                int charCount = editable.length();
+                if (charCount > 0) {
+                    editable.delete(charCount - 1, charCount);
+                }
+            }
+            case R.id.t9_key_submin: { // handle backspace button
+
+                   new GetPassword().execute(mPasswordField.getText().toString());
+            }
+
+            break;
+        }
+    }
+    protected <T extends View> T $(@IdRes int id) {
+        return (T) super.findViewById(id);
+    }
+
 
     private class GetPassword extends AsyncTask<String,Void,String>{
 
@@ -101,9 +163,9 @@ public class PasswordActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-            if (s != "") {
+            if (s.contains("OK")) {
 
-                Toast toast = Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_LONG);
                 toast.show();
 
 
